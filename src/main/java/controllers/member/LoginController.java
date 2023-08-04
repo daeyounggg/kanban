06680e295b4d37a303a6662090b2589b6b5ed4ae
daeyounggg <kdyzab@gmail.com>
2023-08-04 21:51:37 +0900

    의존성 추가

---
+++ src/main/java/controllers/member/LoginController.java
@@ -0,0 +1,15 @@
+package controllers.member;
+
+import controllers.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginController implements Controller  {
+    public void get(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+    public void post(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+}
---
+++ src/main/java/models/works/Status.java
@@ -0,0 +1,8 @@
+package models.works;
+
+public enum Status {
+    READY,
+    PROGRASS,
+    DONE,
+    POSTPONE
+}
