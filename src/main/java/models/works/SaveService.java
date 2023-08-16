eb1e4e75128079b9a2447dc19e7b0c7f9d74ea03
daeyounggg <kdyzab@gmail.com>
2023-08-16 21:52:00 +0900

    오늘 작업 정리

--- src/main/java/models/works/SaveService.java
+++ src/main/java/models/works/SaveService.java
@@ -1,7 +1,11 @@
 package models.works;
 
+import commons.MemberUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import models.member.Users;
 import validators.Validator;
+
+import java.lang.reflect.Member;
 
 public class SaveService {
     private WorkDao workDao;
@@ -34,7 +38,15 @@ public class SaveService {
 
         String workNo = req.getParameter("workNo");
         if(workNo != null && !workNo.isBlank()){
-            work.setWorkNo(Long.parseLong(workNo));
+            long _workNo = Long.parseLong(workNo);
+            work.setWorkNo(_workNo);
+            Work _work = workDao.get(_workNo);
+            MemberUtil.isMine(req, _work.getUserNo());
+        }
+
+        if (MemberUtil.isLogin(req)){ // 로그인한 경우 회원번호 DB에 기록
+            Users users = MemberUtil.getUser(req);
+            work.setUserNo(users.getUserNo());
         }
 
         save(work);
